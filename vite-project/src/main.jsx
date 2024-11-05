9381bcb6ac301c18c0506b19ebbf5fbbe58e4c86
damassetiadi99 <damassetiadi999@gmail.com>
2024-11-06 00:06:59 +0700

    setup main

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -6,9 +6,10 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-  <Provider store={store}>
-      <App />
-    </Provider>
-    </StrictMode>
+   
+     <Provider store={store}>
+       <StrictMode>
+        <App />
+        </StrictMode>
+      </Provider>
 )
